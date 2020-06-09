879cd86441f6bb82ad5f2eb54462024a31d141a8
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-09 15:35:37 +0800

    Attempt '2nd Largest Item BST' problem

---
+++ 2ndLargestIteminBST.js
@@ -0,0 +1,154 @@
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insertLeft(value) {
+        this.left = new BinaryTreeNode(value);
+        return this.left;
+    }
+
+    insertRight(value) {
+        this.right = new BinaryTreeNode(value);
+        return this.right;
+    }
+}
+
+
+
+
+function findLargest(rootNode) {
+    if (!rootNode) {
+        throw new Error('Tree must have at least 1 node');
+    }
+    if (rootNode.right) {
+        return findLargest(rootNode.right);
+    }
+    return rootNode.value;
+}
+
+function findSecondLargest(rootNode) {
+    if (!rootNode || (!rootNode.left && !rootNode.right)) {
+        throw new Error('Tree must have at least 2 nodes');
+    }
+
+    // Case: we're currently at largest, and largest has a left subtree,
+    // so 2nd largest is largest in said subtree
+    if (rootNode.left && !rootNode.right) {
+        return findLargest(rootNode.left);
+    }
+
+    // Case: we're at parent of largest, and largest has no left subtree,
+    // so 2nd largest must be current node
+    if (
+        rootNode.right
+        && !rootNode.right.left
+        && !rootNode.right.right
+    ) {
+        return rootNode.value;
+    }
+
+    // Otherwise: step right
+    return findSecondLargest(rootNode.right);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'full tree';
+let treeRoot = new BinaryTreeNode(50);
+let leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+let rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+rightNode.insertRight(80);
+assertEquals(findSecondLargest(treeRoot), 70, desc);
+
+desc = 'largest has a left child';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+assertEquals(findSecondLargest(treeRoot), 60, desc);
+
+desc = 'largest has a left subtree';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+leftNode = rightNode.insertLeft(60);
+leftNode.insertRight(65);
+leftNode = leftNode.insertLeft(55);
+leftNode.insertRight(58);
+console.log(findSecondLargest(treeRoot))
+assertEquals(findSecondLargest(treeRoot), 65, desc);
+
+desc = 'second largest is root node';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+assertEquals(findSecondLargest(treeRoot), 50, desc);
+
+desc = 'descending linked list';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(40);
+leftNode = leftNode.insertLeft(30);
+leftNode = leftNode.insertLeft(20);
+leftNode = leftNode.insertLeft(10);
+assertEquals(findSecondLargest(treeRoot), 40, desc);
+
+desc = 'ascending linked list';
+treeRoot = new BinaryTreeNode(50);
+rightNode = treeRoot.insertRight(60);
+rightNode = rightNode.insertRight(70);
+rightNode = rightNode.insertRight(80);
+console.log(findSecondLargest(treeRoot))
+assertEquals(findSecondLargest(treeRoot), 70, desc);
+
+desc = 'one node tree';
+treeRoot = new BinaryTreeNode(50);
+assertThrowsError(() => findSecondLargest(treeRoot), desc);
+
+desc = 'when tree is empty';
+treeRoot = null;
+assertThrowsError(() => findSecondLargest(treeRoot), desc);
+
+function assertEquals(a, b, desc) {
+    if (a === b) {
+        console.log(`${desc} ... PASS`);
+    } else {
+        console.log(`${desc} ... FAIL: ${a} != ${b}`)
+    }
+}
+
+function assertThrowsError(func, desc) {
+    try {
+        func();
+        console.log(`${desc} ... FAIL`);
+    } catch (e) {
+        console.log(`${desc} ... PASS`);
+    }
+}
